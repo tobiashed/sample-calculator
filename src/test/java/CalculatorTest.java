90b5b6eca24caee0612d9813e01e8cbb0c38a0bb
Tobias Hedkvist <tobias.hedkvist@hotmail.com>
2017-11-21 10:15:34 +0100

    added all test cases for metod add in class calulator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -61,5 +61,67 @@ public class CalculatorTest {
 			
 		}
 	}
+	@Test
+	public void testAddZeros() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			result = firstNumber + secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.add(firstNumber, secondNumber)), Math.round(result));
+			
+		}
+	}
+	@Test
+	public void testAddSmallSizedNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*-10));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*-10));
+			result = firstNumber + secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.add(firstNumber, secondNumber)), Math.round(result));
+			
+		}
+	}
+	@Test
+	public void testAddMediumSizedNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*-100));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*-100));
+			result = firstNumber + secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.add(firstNumber, secondNumber)), Math.round(result));
+			
+		}
+	}
+	@Test
+	public void testAddLargeSizedNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*-1000));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*-1000));
+			result = firstNumber + secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.add(firstNumber, secondNumber)), Math.round(result));
+			
+		}
+	}
 
 }
